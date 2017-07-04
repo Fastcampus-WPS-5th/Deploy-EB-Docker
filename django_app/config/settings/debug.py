1ec5a8e0b0a4286258864b3ad6ab37108ecfae1d
Lee <dev@azelf.com>
2017-07-04 11:16:18 +0900

    First commit

---
+++ django_app/config/settings/debug.py
@@ -0,0 +1,18 @@
+# debug.py
+from .base import *
+
+config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
+
+# WSGI application
+WSGI_APPLICATION = 'config.wsgi.debug.application'
+
+# Static URLs
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+# 디버그모드니까 DEBUG는 True
+DEBUG = True
+ALLOWED_HOSTS = config_secret_debug['django']['allowed_hosts']
+
+print('@@@@@@ DEBUG:', DEBUG)
+print('@@@@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
---
+++ django_app/config/settings/deploy.py
@@ -0,0 +1,18 @@
+# deploy.py
+from .base import *
+
+config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
+
+# WSGI application
+WSGI_APPLICATION = 'config.wsgi.deploy.application'
+
+# Static URLs
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+# 배포모드니까 DEBUG는 False
+DEBUG = False
+ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
+
+print('@@@@@@ DEBUG:', DEBUG)
+print('@@@@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
