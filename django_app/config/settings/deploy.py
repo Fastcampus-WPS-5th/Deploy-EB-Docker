2ab865fcf416f488e6ac4280b599a7c0c289304d
Lee <dev@azelf.com>
2017-07-05 12:11:49 +0900

    deploy설정변경

--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -11,13 +11,16 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # 배포모드니까 DEBUG는 False
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
 
 # Database
 DATABASES = config_secret_deploy['django']['databases']
 
-
 print('@@@@@@ DEBUG:', DEBUG)
 print('@@@@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
-print(DATABASES)
+
+
+# 1. RDS연동 후 데이터 들어가는지 확인
+#    DJANGO_SETTINGS_MODULE=config.settings.deploy설정
+#    createsuperuser커맨드 실행 후 pgAdmin으로 auth_user테이블에 데이터가 들어갔는지 확인
